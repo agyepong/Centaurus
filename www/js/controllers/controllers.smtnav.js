e3d4dc84fdc85d99af9da83da42b1adad49a6d79
agyepong <stephenagyepong@gmail.com>
2016-02-13 08:07:18 -0500

    added initial sidemenu tab changes

---
+++ www/js/controllers/controllers.smtnav.js
@@ -0,0 +1,11 @@
+﻿angular.module('starter.controllers.smtnav', [])
+.controller('NavCtrl', function ($scope, $ionicSideMenuDelegate) {
+    $scope.showMenu = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+    $scope.showRightMenu = function () {
+        $ionicSideMenuDelegate.toggleRight();
+    };
+})
+
+.controller('SettingsCtrl', function ($scope) { });
